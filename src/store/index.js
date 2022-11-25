e0ac8cc61544c904ff96971ef189fdf6cf11be7a
ibrahimakin <ibrahim-akin@msn.com>
2022-11-25 08:20:55 +0300

    update project

---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+    state: {
+        sampleBlogCards: [
+            { title: "Blog Card #1", photo: "stock-1", date: "May 1, 2021" },
+            { title: "Blog Card #2", photo: "stock-2", date: "May 1, 2021" },
+            { title: "Blog Card #3", photo: "stock-3", date: "May 1, 2021" },
+            { title: "Blog Card #4", photo: "stock-4", date: "May 1, 2021" }
+        ],
+        edit: null
+    },
+    mutations: {
+        toggleEdit(state, payload) {
+            state.edit = payload;
+        }
+    },
+    actions: {
+
+    },
+    getters: {
+        blogPosts(state) {
+            return state.sampleBlogCards;
+        },
+        blogCards(state) {
+            return state.sampleBlogCards;
+        },
+        edit(state) {
+            return state.edit;
+        }
+    }
+});
